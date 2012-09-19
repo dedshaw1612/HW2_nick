03a0a0306ee1d25b9d969c311cf86e8b3db01b9f
Nicholas Uth <uthnp@muohio.edu>
2012-09-19 17:20:03 -0400

    Initial Commit

---
+++ src/HW2_NickApp.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class HW2_NickApp : public AppBasic {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+};
+
+void HW2_NickApp::setup()
+{
+}
+
+void HW2_NickApp::mouseDown( MouseEvent event )
+{
+}
+
+void HW2_NickApp::update()
+{
+}
+
+void HW2_NickApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP_BASIC( HW2_NickApp, RendererGl )
